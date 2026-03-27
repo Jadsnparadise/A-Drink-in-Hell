8410f2d07a9cb8dfd70a0fcac45caf7ebad37948
Jonas Sena <jonas.sena.106@ufrn.edu.br>
2026-03-27 20:06:22 -0300

    Add moveable obstacle finished

--- Assets/Scripts/Map/Obstacles/MoveableObstacle.cs
+++ Assets/Scripts/Map/Obstacles/MoveableObstacle.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using DG.Tweening;
 
-public class MoveableObstacle : MonoBehaviour
+public class MoveableObstacle : Obstacle
 {
     [SerializeField] private Transform endPoint;
     [SerializeField] private float duration = 2f;
@@ -11,5 +11,21 @@ public class MoveableObstacle : MonoBehaviour
         transform.DOMove(endPoint.position, duration)
             .SetEase(Ease.InOutSine)
             .SetLoops(-1, LoopType.Yoyo);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.parent = this.transform;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.parent = null;
+        }
     }
 }
