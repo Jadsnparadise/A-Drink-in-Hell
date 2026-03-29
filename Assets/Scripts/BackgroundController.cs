bc6cdf3d346eceffac58fa32ff2b2fdcc214a25c
Kael Paraguassu <kaelparaguassu@hotmail.com>
2026-03-29 00:25:52 -0300

    Corrigindo bug do portal e melhorando background.

--- Assets/Scripts/BackgroundController.cs
+++ Assets/Scripts/BackgroundController.cs
@@ -1,52 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BackgroundController : MonoBehaviour
 {
+
+    [SerializeField] private bool loopX;
+    [SerializeField] private bool loopY;
+    [SerializeField] private bool centralize;
+
     private float startPosX, startPosY;
     private float lengthX, lengthY;
+    private float camStartPosX, camStartPosY;
 
     public GameObject cam;
     public float parallaxEffect;
     void Start()
     {
-        //startPosX = transform.position.x;
-        //startPosY = transform.position.y;
+        camStartPosX = cam.transform.position.x;
+        camStartPosY = cam.transform.position.y;
+
+        if (centralize)
+        {
+            startPosX = camStartPosX;
+            startPosY = camStartPosY;
+        }
+        else
+        {
+            startPosX = transform.position.x;
+            startPosY = transform.position.y;
+        }
 
         SpriteRenderer sr = GetComponent<SpriteRenderer>();
         lengthX = sr.bounds.size.x;
         lengthY = sr.bounds.size.y;
-
-        startPosX = cam.transform.position.x;
-        startPosY = cam.transform.position.y;
 
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        float distanceX = cam.transform.position.x * parallaxEffect;
-        float distanceY = cam.transform.position.y * parallaxEffect;
+        float distanceX, distanceY;
+        float movementX, movementY;
 
-        float movementX = cam.transform.position.x * (1 - parallaxEffect);
-        float movementY = cam.transform.position.y * (1 - parallaxEffect);
+        if (loopX || loopY)
+        {
+            distanceX = cam.transform.position.x * parallaxEffect;
+            distanceY = cam.transform.position.y * parallaxEffect;
+
+            movementX = cam.transform.position.x * (1 - parallaxEffect);
+            movementY = cam.transform.position.y * (1 - parallaxEffect);
+
+        }
+        else
+        {
+            float relativeCamDistX = cam.transform.position.x - camStartPosX;
+            float relativeCamDistY = cam.transform.position.y - camStartPosY;
+
+            distanceX = relativeCamDistX * parallaxEffect;
+            distanceY = relativeCamDistY * parallaxEffect;
+
+            movementX = relativeCamDistX * (1 - parallaxEffect);
+            movementY = relativeCamDistY * (1 - parallaxEffect);
+
+        }
 
         transform.position = new Vector3(
             startPosX + distanceX,
-            startPosY + distanceY,
+            loopY ? startPosY + distanceY : transform.position.y,
             transform.position.z
         );
 
-        if (movementX > startPosX + lengthX * 0.5f)
-            startPosX += lengthX;
-        else if (movementX < startPosX - lengthX * 0.5f)
-            startPosX -= lengthX;
+        if (loopX)
+        {
+            if (movementX > startPosX + lengthX * 0.5f)
+                startPosX += lengthX;
+            else if (movementX < startPosX - lengthX * 0.5f)
+                startPosX -= lengthX;
+        }
 
-        if (movementY > startPosY + lengthY)
-            startPosY += lengthY;
-        else if (movementY < startPosY - lengthY)
-            startPosY -= lengthY;
+        if (loopY)
+        {
+            if (movementY > startPosY + lengthY)
+                startPosY += lengthY;
+            else if (movementY < startPosY - lengthY)
+                startPosY -= lengthY;
+        }
 
     }
 }
--- Assets/Scripts/Map/PortalController.cs
+++ Assets/Scripts/Map/PortalController.cs
@@ -6,29 +6,38 @@ public class PortalController : MonoBehaviour
     [SerializeField] private GameObject floorLeft;
     [SerializeField] private GameObject floorRight;
 
-    [Header("Configurações")]
+    [Header("Configuracoes")]
     [SerializeField] private float openDistance = 5f;
     [SerializeField] private float duration = 1.2f;
     [SerializeField] private float offsetPlayer = 1f;
-    [SerializeField] private Ease easeType = Ease.InOutQuad; 
+    [SerializeField] private Ease easeType = Ease.InOutQuad;
+
+    private float floorLeftInitialX;
+    private float floorRightInitialX;
+
+    void Start()
+    {
+        floorLeftInitialX = floorLeft.transform.position.x;
+        floorRightInitialX = floorRight.transform.position.x;
+    }
 
     public void OpenPortal()
     {
-        transform.position = new Vector2(PlayerController.Instance.transform.position.x + offsetPlayer, transform.position.y);
+        //transform.position = new Vector2(PlayerController.Instance.transform.position.x + offsetPlayer, transform.position.y);
 
-        floorLeft.transform.DOMoveX(floorLeft.transform.position.x - openDistance, duration)
+        floorLeft.transform.DOMoveX(floorLeftInitialX - openDistance, duration)
             .SetEase(easeType);
 
-        floorRight.transform.DOMoveX(floorRight.transform.position.x + openDistance, duration)
-            .SetEase(easeType);
+        //floorRight.transform.DOMoveX(floorRight.transform.position.x + openDistance, duration)
+        //    .SetEase(easeType);
     }
 
     public void ClosePortal()
     {
-        floorLeft.transform.DOMoveX(floorLeft.transform.position.x + openDistance, duration)
+        floorLeft.transform.DOMoveX(floorLeftInitialX + openDistance, duration)
             .SetEase(easeType);
 
-        floorRight.transform.DOMoveX(floorRight.transform.position.x - openDistance, duration)
-            .SetEase(easeType);
+        //floorRight.transform.DOMoveX(floorRight.transform.position.x - openDistance, duration)
+        //    .SetEase(easeType);
     }
 }
